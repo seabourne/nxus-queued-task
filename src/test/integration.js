d5b5544936ac7db5fd66c80b3ce9b89ef3e04ec0
Luke Opperman <luke@opperman.net>
2018-10-23 15:18:39 -0400

    requestLogin is same as in tester

--- src/test/integration.js
+++ src/test/integration.js
@@ -7,29 +7,12 @@ async function createUser (email, password = 'test', admin=false) {
   return User.findOrCreate({email}, {email, password, admin, enabled: true})
 }
 
-async function requestLogin (email, password = 'test', request = tester.request) {
-  var jar = request.jar()
-  var req = request.defaults({jar: jar})
-  req.cookieJar = jar
-  await req.post({
-    url: 'login',
-    form: {
-      username: email,
-      password: password
-    },
-    simple: false,
-    followRedirect: false
-  })
-  return req
-}
-
-
 let req, taskID
 describe('Integration', () => {
   beforeAll(async () => {
     let email = 'test@example.com'
     let user = await createUser(email)
-    req = await requestLogin(email)
+    req = await tester.requestLogin(email)
     req = req.defaults({resolveWithFullResponse: true, simple: false})
   })
   it('Route responds to initial request by creating task', async () => {
